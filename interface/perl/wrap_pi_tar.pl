690c76273ba7e1836b3ced9cccbd2faa22c582a8
cwant <cwant@95927818-4bc6-11de-b25e-a75f7371a613>
2008-07-10 21:12:02 +0000

    Three things:
    1) Comment formatting got mangled from copy paste from wiki.
    2) Test of post-commit email
    3) Please note that some files have been renamed, so you should
    do a "svn up" immediately:
    
     wrap_pi_tar ==> wrap_pi_tar.pl
     wrap_pi_no_tar.pl ==> wrap_pi_no_tar.pl

--- interface/perl/wrap_pi_tar.pl
+++ interface/perl/wrap_pi_tar.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/perl
-# This script creates a .pbs file, tars the pbs and input file and sends it to the cluster to be run. # pi is a C program that estimates pi based on the number of intervals which is provided in pi.input # It makes assumptions about the order of the command line arguments and has no parsing
+# This script creates a .pbs file, tars the pbs and input file and sends it to the cluster to be run.
+# pi is a C program that estimates pi based on the number of intervals which is provided in pi.input.
+# It makes assumptions about the order of the command line arguments and has no parsing.
 
-# run using perl wrap_pi_tar.pl pvmem=2gb nodes=1:ppn=1 walltime=00:01:00 pi pi.input
+# Run using: 
+#
+#   perl wrap_pi_tar.pl pvmem=2gb nodes=1:ppn=1 walltime=00:01:00 pi pi.input
 
 
 $numArgs = $#ARGV + 1;
