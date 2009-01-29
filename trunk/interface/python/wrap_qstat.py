f6c866e3ccd099e3789fc4557161a71b109f0d9c
cwant <cwant@95927818-4bc6-11de-b25e-a75f7371a613>
2009-01-29 22:18:05 +0000

    * Moved ssh key options to "account options" * first stab at wrap_qstat.py

---
+++ trunk/interface/python/wrap_qstat.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#
+# $Id$
+#
+# Copyright (c) 2009, Chris Want, Research Support Group,
+# AICT, University of Alberta. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+#
+# 1) Redistributions of source code must retain the above copyright 
+#    notice, this list of conditions and the following disclaimer.
+# 2) Redistributions in binary form must reproduce the above copyright 
+#    notice, this list of conditions and the following disclaimer in the 
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+# THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Contributors: Chris Want (University of Alberta),
+#
+
+def main():
+    import os, optparse, PBSUtil
+
+    config = PBSUtil.get_config()
+
+    if (os.getenv("SSH_AGENT_RESPAWN")):
+        # We have been respawned, load pickled options
+        (options, args) = PBSUtil.load_program_args()
+    else:
+        parser = optparse.OptionParser()
+        PBSUtil.add_account_options(parser)
+        (options_obj, args) = parser.parse_args()
+        options = PBSUtil.obj_to_dict(options_obj)
+
+    PBSUtil.validate_host(config, options, options["host"])
+
+    PBSUtil.set_up_ssh(options, args)
+
+    PBSUtil.run_qstat(options)
+
+main()
