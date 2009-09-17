ffd636206deea574ed6ce51dde21921c3f3360df
cjwant <cjwant@95927818-4bc6-11de-b25e-a75f7371a613>
2009-09-17 15:29:35 +0000

    Test commit emails to mailing list which has been broken since July. (Updated the membership of codesite-noreply@google.com to allow posting to the mailing list).

--- trunk/eSaran.py
+++ trunk/eSaran.py
@@ -26,7 +26,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
 # THE POSSIBILITY OF SUCH DAMAGE.
 #
-#
 # Contributors: Chris Want (University of Alberta),
 #               Bart Hazes (University of Alberta)
 #
